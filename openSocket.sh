a4711f7fd24c03702b42447f45e5ad18965c7924
HighBrowInTown <pratikpoojary124@outlook.com>
2023-03-28 23:55:26 +0530

    Minor updates

--- openSocket.sh
+++ openSocket.sh
@@ -2,17 +2,22 @@
 
 DST_IP="${1}"
 PORT="${2}"
+PID=$$
 
 HELP () {
 
-    echo "${0} <DESTINATION IPADDRESS> <PORT>"    
+    echo "${0} <DESTINATION IP ADDRESS> <PORT>"    
 }
 
 VARIABLE_CHK () {
 
-    [ "x${DST_IP}" == "x" ] && echo "Error: Undefined Destination IP" && HELP && exit
-    [ "x${PORT}" == "x" ] && echo "Error: Undefine PORT" && HELP && exit
+    [ "x${DST_IP}" == "x" ] && echo "Error: Undefined Destination IP" && HELP && exit 1
+    [ "x${PORT}" == "x" ] && echo "Error: Undefine PORT" && HELP && exit 1
+}
 
+INFO () {
+
+    echo "ALL traffic received on port ${PORT} will be forwarded to ${DST_IP}:${PORT}"
 }
 
 OPEN_PORT () {
@@ -21,16 +26,7 @@ OPEN_PORT () {
     do 
         socat tcp4-listen:"${PORT}",reuseaddr tcp:"${DST_IP}:${PORT}" > /dev/null & disown
     done
-
 }
-
-INFO () {
-
-    echo "ALL traffic received on port ${PORT} will be forwarded to ${DST_IP}:${PORT}"
-    
-}
-
-
 
 MAIN () {
 
